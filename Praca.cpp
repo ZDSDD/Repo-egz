f6d1f5dfb491bf46f75e378e5e97f6b97ea17c55
ZDSDD <sebasadowy@gmail.com>
2022-06-12 15:37:30 +0200

    Updated README file and added Praca.cpp
    Praca.cpp is the main file

---
+++ Praca.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <random> // testing library for better randomness
+#include "../Podatnik.h"
+
+using namespace std;
+
+int main(){
+
+/* Settings to number generator */
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> dist(0,9);
+/* End of setting */
+
+    string NIP="00000000000"; //11 digits
+    string CompanyName;
+
+    int Companies_to_add=0;
+    cout << "Enter number of companies you want to add(0 for skip): "; cin >> Companies_to_add; cin.ignore();
+    ///Start work with file
+    if(Companies_to_add > 0) {
+        ofstream File_Taxpayers("MyData.txt", ios::app);
+        if (File_Taxpayers.good()) {
+            int i, j;
+            for (i = 0, j = 0; i < Companies_to_add; i++, j = 0) {
+                NIP.replace(j, 1, to_string(dist(gen)));
+                if (NIP[0] == '0')NIP[0] = '1';
+                for (j = 1; j < 11; j++) {
+                    NIP.replace(j, 1, to_string(dist(gen)));
+                }
+                cout << "Enter company name: ";
+                getline(cin, CompanyName);
+                File_Taxpayers << NIP << " " << CompanyName << "\n";
+            }
+        }else cout << "Couldn't open the file" << endl;
+        File_Taxpayers.close();
+        ///File close, end input
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Repo-egz
+
+This is repo for c++ exam. Mostly working with files and data structures
